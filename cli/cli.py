3c10fbd22c191ac61d8ff771f6975565e7364de1
Balaj_Muben <mbalajmubeen@gmail.com>
2025-12-14 04:34:48 +0500

    Still working

--- cli/cli.py
+++ cli/cli.py
@@ -2,19 +2,26 @@ import pyfiglet , argparse
 # import encoding as encode
 
 
-tool_name = pyfiglet.figlet_format("Crypto-Toolkit", font="Standard")
+# tool_name = pyfiglet.figlet_format("Crypto-Toolkit", font="Standard")
 # print(tool_name)
 # print(encode.base64_encoding(input("enter something to encode in Base32 format: ")))
 
-def phrases():
-    parser = argparse.ArgumentParser(
-                    prog='cyber-Toolkit',
-                    description='can turn your data into any from of encryption or hashes',
-                    epilog='Help here')
-    parser.add_argument('filename')           # positional argument
-    parser.add_argument('-c', '--count')      # option that takes a value
-    parser.add_argument('-v', '--verbose',
-                    action='store_true')  # on/off flag
+parser = argparse.ArgumentParser(
+            prog='cyber-Toolkit',
+            description='can turn your data into any from of encryption or hashes')
+parser.add_argument('-e','--encode',help='encode the data',
+                     choices=["b64", "b32"])
 
-while True:
-    phrases()
+
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    if args.encode:
+        if args.b64:
+            print("Base64 encoding")
+        elif args.b32:
+            print("Base32 encoding")
+        else:
+            parser.error("-e requires -b64 or -b32")
--- encoding/urlsafe.py
+++ encoding/urlsafe.py
@@ -1,6 +1,4 @@
 import base64
-from base64 import decode
-
 
 def urlsafe_encoding(text: str) -> str:
     return base64.urlsafe_b64encode(text.encode()).decode()
@@ -9,4 +7,3 @@ def urlsafe_decoding(text: str) -> str:
     return base64.urlsafe_b64encode(text).decode()
 
 
-# print(urlsafe_decoding("bQ=="))
