17e6fbf9b2e6933e62384f494e91982550c67c3b
Balaj_Muben <mbalajmubeen@gmail.com>
2025-12-14 04:58:57 +0500

    correcting requestes methods

--- Web/web.py
+++ Web/web.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST","PUT"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     selected_encoding = None
     input_text = ""
