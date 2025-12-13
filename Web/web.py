e93c5f3a48a1e685a2e5edb9a70c887f11440214
Balaj_Muben <mbalajmubeen@gmail.com>
2025-12-14 04:57:47 +0500

    design and layout

---
+++ Web/web.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST","PUT"])
+def index():
+    selected_encoding = None
+    input_text = ""
+
+    if request.method == "POST":
+        selected_encoding = request.form.get("encoding")
+        input_text = request.form.get("text")
+
+
+    return render_template(
+        "index.html",
+        encoding=selected_encoding,
+        text=input_text
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
