e32c451bc9955da6d612e5883a5c496332eb4341
Balaj_Muben <mbalajmubeen@gmail.com>
2025-12-13 19:11:58 +0500

    still working

--- cli/cli.py
+++ cli/cli.py
@@ -1,9 +1,20 @@
 import pyfiglet , argparse
-import encoding as encode
+# import encoding as encode
 
 
 tool_name = pyfiglet.figlet_format("Crypto-Toolkit", font="Standard")
-print(tool_name)
-print(encode.base64_encoding(input("enter something to encode in Base32 format: ")))
+# print(tool_name)
+# print(encode.base64_encoding(input("enter something to encode in Base32 format: ")))
 
-# def phrases():
+def phrases():
+    parser = argparse.ArgumentParser(
+                    prog='cyber-Toolkit',
+                    description='can turn your data into any from of encryption or hashes',
+                    epilog='Help here')
+    parser.add_argument('filename')           # positional argument
+    parser.add_argument('-c', '--count')      # option that takes a value
+    parser.add_argument('-v', '--verbose',
+                    action='store_true')  # on/off flag
+
+while True:
+    phrases()
