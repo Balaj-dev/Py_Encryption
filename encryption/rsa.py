529c8dec82288a3822998778fb6d2131d54c70b5
Balaj_Muben <mbalajmubeen@gmail.com>
2025-12-25 19:55:48 +0500

    hmmm study about RSA

--- encryption/rsa.py
+++ encryption/rsa.py
@@ -1,1 +1,7 @@
-import cryptography
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048)
+
+print(private_key)
