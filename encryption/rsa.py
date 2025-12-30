61160bcf1e6f38211215e0436e837407287ffe52
Balaj_Muben <mbalajmubeen@gmail.com>
2025-12-31 01:30:19 +0500

    hmmm somechanges a bit maybe

--- encryption/rsa.py
+++ encryption/rsa.py
@@ -1,10 +1,10 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 
-# private_key = rsa.generate_private_key(
-#     public_exponent=65537,
-#     key_size=2048)
-#
-# print(private_key)
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048)
+
+print(private_key)
 
 def encrypted_rsa():
     return
